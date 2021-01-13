cb25a2ccae3e4be3caa5eacd201ebb9bede13055
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-13 14:59:26 -0500

    Review:Learned constants

--- experiments/review-experiments/js/script.js
+++ experiments/review-experiments/js/script.js
@@ -1,20 +1,33 @@
 /**************************************************
 Template p5 project
-Pippin Barr
+Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-
+"use strict";
+const NUM_CIRCLES = 10;
+let circleAlpha = 50;
+let circleSizeIncrease = 50;
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  circleAlpha = map(mouseX, 0, width, 10, 100);
+  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+
+  for (let i = 0; i < NUM_CIRCLES; i++) {
+    push();
+    fill(255, circleAlpha);
+    ellipse(width / 2, height / 2, i * circleSizeIncrease);
+    pop();
+  }
 }
