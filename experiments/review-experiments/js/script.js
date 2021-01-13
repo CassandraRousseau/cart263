2528ba4aef40e64df27c53fa4d2cd0847e9edce3
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-13 15:17:56 -0500

    Review: Learned object parameters

--- experiments/review-experiments/js/script.js
+++ experiments/review-experiments/js/script.js
@@ -5,9 +5,7 @@ Cassandra Rousseau
 Here is a description of this template p5 project.
 **************************************************/
 "use strict";
-const NUM_CIRCLES = 10;
-let circleAlpha = 50;
-let circleSizeIncrease = 50;
+
 // setup()
 //
 // Description of setup() goes here.
@@ -20,14 +18,25 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+  let config = {
+    x: 250,
+    y: 250,
+    width: 200,
+    height: 200,
+    fillColor: {
+      r: 255,
+      g: 255,
+      b: 0,
+    },
 
-  circleAlpha = map(mouseX, 0, width, 10, 100);
-  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
-
-  for (let i = 0; i < NUM_CIRCLES; i++) {
-    push();
-    fill(255, circleAlpha);
-    ellipse(width / 2, height / 2, i * circleSizeIncrease);
-    pop();
-  }
+    mode: CENTER,
+  };
+  drawFancyRect(config);
+}
+function drawFancyRect({ x, y, width, height, fillColor, mode }) {
+  push();
+  fill(fillColor.r, fillColor.g, fillColor.b);
+  rectMode(mode);
+  rect(x, y, width, height);
+  pop();
 }
