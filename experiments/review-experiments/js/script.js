5af203b3bd09e1802f6cadc9888601f92fa4cdb3
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-13 15:44:01 -0500

    Review:Learned first-class functions

--- experiments/review-experiments/js/script.js
+++ experiments/review-experiments/js/script.js
@@ -6,37 +6,6 @@ Here is a description of this template p5 project.
 **************************************************/
 "use strict";
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-  createCanvas(500, 500);
-}
-
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
-  background(0);
-  let config = {
-    x: 250,
-    y: 250,
-    width: 200,
-    height: 200,
-    fillColor: {
-      r: 255,
-      g: 255,
-      b: 0,
-    },
-
-    mode: CENTER,
-  };
-  drawFancyRect(config);
-}
-function drawFancyRect({ x, y, width, height, fillColor, mode }) {
-  push();
-  fill(fillColor.r, fillColor.g, fillColor.b);
-  rectMode(mode);
-  rect(x, y, width, height);
-  pop();
-}
+setTimeout(function () {
+  alert(`Hello!`);
+}, 5000);
