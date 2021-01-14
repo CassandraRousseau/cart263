5e202c24d23516552dfe33ea15abd5539864160b
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-14 16:32:30 -0500

    A2:Created sausage dog class

---
+++ activities/02-wheres-sausage-dog/js/SausageDog.js
@@ -0,0 +1,29 @@
+class SausageDog extends Animal {
+  //Creating the second cutscene of Chapter Five
+  constructor(x, y, width, height, image) {
+    super(x, y, width, height, image);
+
+    this.found = false;
+    this.rotationSpeed = 0.25;
+  }
+
+  //Setting the second cutscene of Chapter Five
+  update() {
+    super.update();
+
+    if (this.found) {
+      this.angle += this.rotationSpeed;
+    }
+  }
+
+  mousePressed() {
+    if (
+      mouseX > this.x - this.image.width / 2 &&
+      mouseX < this.x + this.image.width / 2 &&
+      mouseY > this.y - this.image.height / 2 &&
+      mouseY < this.y + this.image.height / 2
+    ) {
+      this.found = true;
+    }
+  }
+}
