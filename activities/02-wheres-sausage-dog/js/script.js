4a95ad330a1f459c0a1ec5a3f92a8c3c01325c14
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-14 17:29:19 -0500

    A2:Improved activity code

--- activities/02-wheres-sausage-dog/js/script.js
+++ activities/02-wheres-sausage-dog/js/script.js
@@ -6,8 +6,11 @@ one of which will be a sausage dog. The player has to click on the sausage dog
 to win the game. When clicked successfully, the sausage dog will start spinning.
 **************************************************/
 "use strict";
+
+const LOADING_SAUSAGE_DOG = "assets/images/sausage-dog.png";
 const NUM_ANIMALS_IMAGES = 10;
 const NUM_ANIMALS_DISPLAY = 100;
+
 let animalImage = [];
 let animalObject = [];
 let dogImage;
@@ -18,35 +21,49 @@ function preload() {
     let loadedImage = loadImage(`assets/images/animal${i}.png`);
     animalImage.push(loadedImage);
   }
-  dogImage = loadImage("assets/images/sausage-dog.png");
+  dogImage = loadImage(LOADING_SAUSAGE_DOG);
 }
-// setup()
-//
-// Description of setup() goes here.
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  createAnimals();
+  createSausageDog();
+}
+
+function createAnimals() {
   for (let i = 0; i < NUM_ANIMALS_DISPLAY; i++) {
-    let x = random(0, width);
-    let y = random(0, height);
-    let loadedImage = random(animalImage);
-    let animal = new Animal(x, y, loadedImage);
-    animalObject.push(animal);
+    randomAnimals();
   }
+}
+
+function createSausageDog() {
   let x = random(0, width);
   let y = random(0, height);
   sausageDog = new SausageDog(x, y, dogImage);
 }
-
-// draw()
-//
-// Description of draw() goes here.
+function randomAnimals() {
+  let x = random(0, width);
+  let y = random(0, height);
+  let loadedImage = random(animalImage);
+  let animal = new Animal(x, y, loadedImage);
+  animalObject.push(animal);
+}
 function draw() {
   background(255);
+  updateAnimals();
+  updateSausageDog();
+}
+
+function updateAnimals() {
   for (let i = 0; i < animalObject.length; i++) {
     animalObject[i].update();
   }
+}
+
+function updateSausageDog() {
   sausageDog.update();
 }
+
 function mousePressed() {
   sausageDog.mousePressed();
 }
