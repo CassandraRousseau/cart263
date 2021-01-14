dd25b4c0e1f037914b0155fae29aa01dafd6cbb7
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-14 14:23:07 -0500

    A2:Added animal images and activity description into the js main script

--- activities/02-wheres-sausage-dog/js/script.js
+++ activities/02-wheres-sausage-dog/js/script.js
@@ -1,20 +1,17 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Activity 02: Where's sausage dog?
+Cassandra Rousseau
+The canvas will display many random images of animals at random positions,
+one of which will be a sausage dog. The player has to click on the sausage dog
+to win the game. When clicked successfully, the sausage dog will start spinning.
 **************************************************/
-
+"use strict";
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
-
-}
+function setup() {}
 
 // draw()
 //
 // Description of draw() goes here.
-function draw() {
-
-}
+function draw() {}
