92454187fc98835fa85ca316245fa8655cf85ea2
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-15 16:05:43 -0500

    Voices: Learned responsiveVoice

--- experiments/voices-experiments/js/script.js
+++ experiments/voices-experiments/js/script.js
@@ -1,20 +1,43 @@
 /**************************************************
 Template p5 project
-Pippin Barr
+Cassandra Rousseau
 
 Here is a description of this template p5 project.
 **************************************************/
-
+"use strict";
+let phrase = "Hello,world!";
+let saying = "";
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(255);
 
+  push();
+  textSize(32);
+  textAlign(CENTER);
+  text(saying, width / 2, height / 2);
+  pop();
+}
+function mousePressed() {
+  responsiveVoice.speak(phrase, "UK English Male", {
+    onstart: showSpeaking,
+    onend: hideSpeaking,
+    pitch: 0.5,
+    rate: 0.5,
+    volume: 1,
+  });
+}
+function showSpeaking() {
+  saying = phrase;
+}
+function hideSpeaking() {
+  saying = "";
 }
