3cd46b27602be7fee6c4a8e077ed8fa6feb8e6cd
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-15 18:59:39 -0500

    A2:Added animals name list; added annyang and responsiveVoice in the code; added reverseString function

--- activities/02-slamina/js/script.js
+++ activities/02-slamina/js/script.js
@@ -1,20 +1,198 @@
 /**************************************************
-Template p5 project
-Pippin Barr
-
-Here is a description of this template p5 project.
+Activity 02:Slamina
+Cassandra Rousseau
+The program will speak the name of a common animal backwards and the user will
+have to say (with their voice) what they think it is in the form “I think it is
+ x.” If they get it right, their guess will be displayed in green, if they get
+ it wrong, their guess will be displayed in red.
 **************************************************/
-
+"use strict";
+const animals = {
+  animals: [
+    "aardvark",
+    "alligator",
+    "alpaca",
+    "antelope",
+    "ape",
+    "armadillo",
+    "baboon",
+    "badger",
+    "bat",
+    "bear",
+    "beaver",
+    "bison",
+    "boar",
+    "buffalo",
+    "bull",
+    "camel",
+    "canary",
+    "capybara",
+    "cat",
+    "chameleon",
+    "cheetah",
+    "chimpanzee",
+    "chinchilla",
+    "chipmunk",
+    "cougar",
+    "cow",
+    "coyote",
+    "crocodile",
+    "crow",
+    "deer",
+    "dingo",
+    "dog",
+    "donkey",
+    "dromedary",
+    "elephant",
+    "elk",
+    "ewe",
+    "ferret",
+    "finch",
+    "fish",
+    "fox",
+    "frog",
+    "gazelle",
+    "gila monster",
+    "giraffe",
+    "gnu",
+    "goat",
+    "gopher",
+    "gorilla",
+    "grizzly bear",
+    "ground hog",
+    "guinea pig",
+    "hamster",
+    "hedgehog",
+    "hippopotamus",
+    "hog",
+    "horse",
+    "hyena",
+    "ibex",
+    "iguana",
+    "impala",
+    "jackal",
+    "jaguar",
+    "kangaroo",
+    "koala",
+    "lamb",
+    "lemur",
+    "leopard",
+    "lion",
+    "lizard",
+    "llama",
+    "lynx",
+    "mandrill",
+    "marmoset",
+    "mink",
+    "mole",
+    "mongoose",
+    "monkey",
+    "moose",
+    "mountain goat",
+    "mouse",
+    "mule",
+    "muskrat",
+    "mustang",
+    "mynah bird",
+    "newt",
+    "ocelot",
+    "opossum",
+    "orangutan",
+    "oryx",
+    "otter",
+    "ox",
+    "panda",
+    "panther",
+    "parakeet",
+    "parrot",
+    "pig",
+    "platypus",
+    "polar bear",
+    "porcupine",
+    "porpoise",
+    "prairie dog",
+    "puma",
+    "rabbit",
+    "raccoon",
+    "ram",
+    "rat",
+    "reindeer",
+    "reptile",
+    "rhinoceros",
+    "salamander",
+    "seal",
+    "sheep",
+    "shrew",
+    "silver fox",
+    "skunk",
+    "sloth",
+    "snake",
+    "squirrel",
+    "tapir",
+    "tiger",
+    "toad",
+    "turtle",
+    "walrus",
+    "warthog",
+    "weasel",
+    "whale",
+    "wildcat",
+    "wolf",
+    "wolverine",
+    "wombat",
+    "woodchuck",
+    "yak",
+    "zebra",
+  ],
+};
+let currentAnimal = "";
+let currentAnswer = "";
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(500, 500);
+  if (annyang) {
+    // Let's define our first command. First the text we expect, and then the function it should call
+    let commands = {
+      "I think is it *animal": guessedAnimal,
+      "I hate you": hate,
+    };
 
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+    annyang.start();
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-
+  background(0);
+  push();
+  translate(width / 2, height / 2);
+  rotate(PI / 2);
+  textSize(400);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text(face, 0, 0);
+  pop();
 }
+function reverseString(string) {
+  // Split the string into an array of characters
+  let characters = string.split("");
+  // Reverse the array of characters
+  let reverseCharacters = characters.reverse();
+  // Join the array of characters back into a string
+  let result = reverseCharacters.join("");
+  // Return the result
+  return result;
+}
+
+function mousePressed() {
+  let currentAnimal = random(animals[i]);
+  let reverseAnimal = reverseString(currentAnimal);
+  responsiveVoice.speak("");
+}
+function guessedAnimal() {}
