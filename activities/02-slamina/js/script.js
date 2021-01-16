2c45df42408c082d131ec2b3dfee50fa9bad76c1
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-01-15 20:16:38 -0500

    A2:Corrected,improved and finished slamina activity

--- activities/02-slamina/js/script.js
+++ activities/02-slamina/js/script.js
@@ -7,144 +7,143 @@ have to say (with their voice) what they think it is in the form “I think it i
  it wrong, their guess will be displayed in red.
 **************************************************/
 "use strict";
-const animals = {
-  animals: [
-    "aardvark",
-    "alligator",
-    "alpaca",
-    "antelope",
-    "ape",
-    "armadillo",
-    "baboon",
-    "badger",
-    "bat",
-    "bear",
-    "beaver",
-    "bison",
-    "boar",
-    "buffalo",
-    "bull",
-    "camel",
-    "canary",
-    "capybara",
-    "cat",
-    "chameleon",
-    "cheetah",
-    "chimpanzee",
-    "chinchilla",
-    "chipmunk",
-    "cougar",
-    "cow",
-    "coyote",
-    "crocodile",
-    "crow",
-    "deer",
-    "dingo",
-    "dog",
-    "donkey",
-    "dromedary",
-    "elephant",
-    "elk",
-    "ewe",
-    "ferret",
-    "finch",
-    "fish",
-    "fox",
-    "frog",
-    "gazelle",
-    "gila monster",
-    "giraffe",
-    "gnu",
-    "goat",
-    "gopher",
-    "gorilla",
-    "grizzly bear",
-    "ground hog",
-    "guinea pig",
-    "hamster",
-    "hedgehog",
-    "hippopotamus",
-    "hog",
-    "horse",
-    "hyena",
-    "ibex",
-    "iguana",
-    "impala",
-    "jackal",
-    "jaguar",
-    "kangaroo",
-    "koala",
-    "lamb",
-    "lemur",
-    "leopard",
-    "lion",
-    "lizard",
-    "llama",
-    "lynx",
-    "mandrill",
-    "marmoset",
-    "mink",
-    "mole",
-    "mongoose",
-    "monkey",
-    "moose",
-    "mountain goat",
-    "mouse",
-    "mule",
-    "muskrat",
-    "mustang",
-    "mynah bird",
-    "newt",
-    "ocelot",
-    "opossum",
-    "orangutan",
-    "oryx",
-    "otter",
-    "ox",
-    "panda",
-    "panther",
-    "parakeet",
-    "parrot",
-    "pig",
-    "platypus",
-    "polar bear",
-    "porcupine",
-    "porpoise",
-    "prairie dog",
-    "puma",
-    "rabbit",
-    "raccoon",
-    "ram",
-    "rat",
-    "reindeer",
-    "reptile",
-    "rhinoceros",
-    "salamander",
-    "seal",
-    "sheep",
-    "shrew",
-    "silver fox",
-    "skunk",
-    "sloth",
-    "snake",
-    "squirrel",
-    "tapir",
-    "tiger",
-    "toad",
-    "turtle",
-    "walrus",
-    "warthog",
-    "weasel",
-    "whale",
-    "wildcat",
-    "wolf",
-    "wolverine",
-    "wombat",
-    "woodchuck",
-    "yak",
-    "zebra",
-  ],
-};
+const animals = [
+  "aardvark",
+  "alligator",
+  "alpaca",
+  "antelope",
+  "ape",
+  "armadillo",
+  "baboon",
+  "badger",
+  "bat",
+  "bear",
+  "beaver",
+  "bison",
+  "boar",
+  "buffalo",
+  "bull",
+  "camel",
+  "canary",
+  "capybara",
+  "cat",
+  "chameleon",
+  "cheetah",
+  "chimpanzee",
+  "chinchilla",
+  "chipmunk",
+  "cougar",
+  "cow",
+  "coyote",
+  "crocodile",
+  "crow",
+  "deer",
+  "dingo",
+  "dog",
+  "donkey",
+  "dromedary",
+  "elephant",
+  "elk",
+  "ewe",
+  "ferret",
+  "finch",
+  "fish",
+  "fox",
+  "frog",
+  "gazelle",
+  "gila monster",
+  "giraffe",
+  "gnu",
+  "goat",
+  "gopher",
+  "gorilla",
+  "grizzly bear",
+  "ground hog",
+  "guinea pig",
+  "hamster",
+  "hedgehog",
+  "hippopotamus",
+  "hog",
+  "horse",
+  "hyena",
+  "ibex",
+  "iguana",
+  "impala",
+  "jackal",
+  "jaguar",
+  "kangaroo",
+  "koala",
+  "lamb",
+  "lemur",
+  "leopard",
+  "lion",
+  "lizard",
+  "llama",
+  "lynx",
+  "mandrill",
+  "marmoset",
+  "mink",
+  "mole",
+  "mongoose",
+  "monkey",
+  "moose",
+  "mountain goat",
+  "mouse",
+  "mule",
+  "muskrat",
+  "mustang",
+  "mynah bird",
+  "newt",
+  "ocelot",
+  "opossum",
+  "orangutan",
+  "oryx",
+  "otter",
+  "ox",
+  "panda",
+  "panther",
+  "parakeet",
+  "parrot",
+  "pig",
+  "platypus",
+  "polar bear",
+  "porcupine",
+  "porpoise",
+  "prairie dog",
+  "puma",
+  "rabbit",
+  "raccoon",
+  "ram",
+  "rat",
+  "reindeer",
+  "reptile",
+  "rhinoceros",
+  "salamander",
+  "seal",
+  "sheep",
+  "shrew",
+  "silver fox",
+  "skunk",
+  "sloth",
+  "snake",
+  "squirrel",
+  "tapir",
+  "tiger",
+  "toad",
+  "turtle",
+  "walrus",
+  "warthog",
+  "weasel",
+  "whale",
+  "wildcat",
+  "wolf",
+  "wolverine",
+  "wombat",
+  "woodchuck",
+  "yak",
+  "zebra",
+];
+
 let currentAnimal = "";
 let currentAnswer = "";
 // setup()
@@ -155,13 +154,16 @@ function setup() {
   if (annyang) {
     // Let's define our first command. First the text we expect, and then the function it should call
     let commands = {
-      "I think is it *animal": guessedAnimal,
-      "I hate you": hate,
+      "I think it is *animal": guessedAnimal,
     };
 
     // Add our commands to annyang
     annyang.addCommands(commands);
     annyang.start();
+
+    textSize(50);
+    textStyle(BOLD);
+    textAlign(CENTER, CENTER);
   }
 }
 
@@ -170,14 +172,14 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
-  push();
-  translate(width / 2, height / 2);
-  rotate(PI / 2);
-  textSize(400);
-  textAlign(CENTER, CENTER);
-  fill(255);
-  text(face, 0, 0);
-  pop();
+  displayAnswer();
+}
+function mousePressed() {
+  nextQuestion();
+}
+
+function guessedAnimal(animal) {
+  currentAnswer = animal.toLowerCase();
 }
 function reverseString(string) {
   // Split the string into an array of characters
@@ -189,10 +191,16 @@ function reverseString(string) {
   // Return the result
   return result;
 }
-
-function mousePressed() {
-  let currentAnimal = random(animals[i]);
-  let reverseAnimal = reverseString(currentAnimal);
-  responsiveVoice.speak("");
+function displayAnswer() {
+  if (currentAnswer === currentAnimal) {
+    fill(0, 255, 0);
+  } else {
+    fill(255, 0, 0);
+  }
+  text(currentAnswer, width / 2, height / 2);
 }
-function guessedAnimal() {}
+function nextQuestion() {
+  let currentAnimal = random(animals);
+  let reverseAnimal = reverseString(currentAnimal);
+  responsiveVoice.speak(reverseAnimal);
+}
