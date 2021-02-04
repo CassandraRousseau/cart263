ee9493e562e6245dfe5b712afd2e37ea3497b92d
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-02-04 11:39:49 -0500

    P1:Created 3D environments for bakery, main hall and auditorium

---
+++ projects/project01/js/Bakery.js
@@ -0,0 +1,39 @@
+function draw() {
+  background(255);
+
+  // Translate to the center (not really needed, but just for completeness)
+
+  // Looks nicer
+  noStroke();
+
+  push();
+
+  // Our central cube is white
+  translate(0, -115, -600);
+  fill(150);
+  texture(windows);
+  box(2000, height, 800);
+  pop();
+  push();
+  // A red bar passing through the box
+  translate(0, 250, 0);
+  fill(100, 185, 225);
+  box(windowWidth, 100, 600);
+  pop();
+  push();
+
+  // Our central cube is white
+  translate(-width / 4, 75, 200);
+  fill(255, 255, 150);
+  box(250, 100, 400);
+  pop();
+
+  push();
+
+  // Our central cube is white
+  translate(width / 4, 75, 200);
+  fill(255, 255, 150);
+
+  box(250, 100, 400);
+  pop();
+}
--- projects/project01/js/script.js
+++ projects/project01/js/script.js
@@ -8,11 +8,15 @@ let grassPurple;
 let grassPink;
 let grassBlue;
 let windows;
+let floor;
+let curtains;
 function preload() {
   windows = loadImage(`assets/images/windows.jpg`);
   grassPurple = loadImage(`assets/images/grassPurple.png`);
   grassPink = loadImage(`assets/images/grassPink.png`);
   grassBlue = loadImage(`assets/images/grassBlue.png`);
+  floor = loadImage(`assets/images/floor.jpg`);
+  curtains = loadImage(`assets/images/curtains.jpg`);
 }
 function setup() {
   // Using WEBGL in createCanvas to specify 3D graphics
@@ -20,60 +24,91 @@ function setup() {
 }
 
 function draw() {
-  background(200);
-
+  background(0);
+  lights();
   // Translate to the center (not really needed, but just for completeness)
 
   // Looks nicer
   noStroke();
   push();
+
+  // Our central cube is white
+  translate(width / 4, -200, -350);
+  fill(160, 50, 75);
+  texture(curtains);
+  plane(1000, height);
+  pop();
+  push();
+
+  // Our central cube is white
+  translate(-width / 4, -200, -350);
+  fill(160, 50, 75);
+  texture(curtains);
+  plane(1000, height);
+  pop();
+
+  push();
+
+  // Our central cube is white
+  translate(0, 100, -750);
+  fill(150);
+
+  ellipsoid(850, 100);
+  pop();
+  push();
   // A red bar passing through the box
   translate(0, 250, 0);
-  fill(100, 185, 225);
-
-  box(windowWidth, 100, 3000);
+  fill(100);
+  box(windowWidth, 100, 600);
   pop();
   push();
 
   // Our central cube is white
-  translate(-width / 3, 75, -500);
-  fill(150);
-  texture(windows);
-  box(300, windowHeight);
-  pop();
+  translate(-width / 4, 100, 100);
+  fill(160, 50, 75);
 
-  push();
-
-  // Our central cube is white
-  translate(-width / 4, 75, -500);
-  fill(150);
-  texture(windows);
-  box(100, 700);
+  box(300, 75, 50);
   pop();
 
   push();
 
   // Our central cube is white
-  translate(-width / 2, 100, -600);
-  fill(150);
-  texture(windows);
-  box(300, 900);
+  translate(width / 4, 100, 100);
+  fill(160, 50, 75);
+
+  box(300, 75, 50);
+  pop();
+  push();
+
+  // Our central cube is white
+  translate(-width / 4, 100, 200);
+  fill(160, 50, 75);
+
+  box(300, 75, 50);
   pop();
 
   push();
 
   // Our central cube is white
-  translate(width / 4, 75, -500);
-  fill(150);
-  texture(windows);
-  box(100, 700);
+  translate(width / 4, 100, 200);
+  fill(160, 50, 75);
+
+  box(300, 75, 50);
   pop();
   push();
 
   // Our central cube is white
-  translate(width / 2, 0, -400);
-  fill(150);
-  texture(windows);
-  box(200, 500, -10);
+  translate(-width / 4, 100, 300);
+  fill(160, 50, 75);
+  box(300, 75, 50);
+  pop();
+
+  push();
+
+  // Our central cube is white
+  translate(width / 4, 100, 300);
+  fill(160, 50, 75);
+
+  box(300, 75, 50);
   pop();
 }
