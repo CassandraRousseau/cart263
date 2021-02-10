140fe9aceb42f882444636589a9c715d7a67db10
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-02-10 12:42:51 -0500

    E3:Added comments

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -7,12 +7,16 @@ When the user first loads our program it will ask for their name in a text promp
    When the user comes back later, they will need to enter their generated password
     to view their profile again.
 **************************************************/
+"use strict";
+//Added JSON data
 const JSON_TAROT = `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`;
 const JSON_DESCRIPTIONS = `https://raw.githubusercontent.com/dariusk/corpora/master/data/humans/descriptions.json`;
 const JSON_SPELL = `https://raw.githubusercontent.com/dariusk/corpora/master/data/words/spells.json`;
 const JSON_OBJECT = `https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`;
 const JSON_INSTRUMENT = `https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`;
 const KEY_PROFILE_DATA = `spy-profile-data`;
+
+//Added spy profile
 let spyProfile = {
   name: "REDACTED",
   alias: "REDACTED",
@@ -21,6 +25,8 @@ let spyProfile = {
   secretWeapon: "REDACTED",
   password: "REDACTED",
 };
+
+//Added dialog in disappointment state
 const disappointed = [
   `How dare you!!!`,
   `Forgetting your agent name and/or your password?!?`,
@@ -64,13 +70,21 @@ const disappointed = [
   `HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH`,
   `HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`,
 ];
+
+//Added data variables
 let tarotData;
 let objectData;
 let descriptionData;
 let spellData;
 let instrumentData;
+
+//Added current line in dialogs
 let currentLine = 0;
+
+//Added state variable
 let state = ``;
+
+//Preloading JSON data
 function preload() {
   tarotData = loadJSON(JSON_TAROT);
   descriptionData = loadJSON(JSON_DESCRIPTIONS);
@@ -78,33 +92,47 @@ function preload() {
   objectData = loadJSON(JSON_OBJECT);
   instrumentData = loadJSON(JSON_INSTRUMENT);
 }
-// setup()
-//
-// Description of setup() goes here.
+
+//Setting the program
 function setup() {
   console.log(state);
+
+  //Setting Canvas
   createCanvas(windowWidth, windowHeight);
+
+  //Setting saved JSON data
   let data = JSON.parse(localStorage.getItem(KEY_PROFILE_DATA));
 
+  //setting what happens with data
   if (data !== null) {
+    //Asking name and password
     spyProfile.name = prompt(`What's your name?`);
     let password = prompt(`What is your password?`);
+
+    //Setting spy data if name and password are right
     if (name === data.name && password === data.password) {
       console.log(`file`);
       state = `file`;
       currentLine = 0;
       setSpyData(spyProfile, data);
-    } else if (name !== data.name || password !== data.password) {
+    }
+
+    //Setting disappointment state if name or password is wrong
+    else if (name !== data.name || password !== data.password) {
       state == `disappointment`;
       currentLine = 0;
     }
-  } else {
+  }
+
+  //Generate a spy profile of there's no data
+  else {
     state = "file";
     currentLine = 0;
     generateSpyProfile();
   }
 }
 
+//Sets the data according to the required information in spy profile
 function setSpyData(spyProfile, data) {
   spyProfile.name = data.name;
   spyProfile.alias = data.alias;
@@ -112,8 +140,9 @@ function setSpyData(spyProfile, data) {
   spyProfile.power = data.power;
   spyProfile.secretWeapon = data.secretWeapon;
   spyProfile.password = data.password;
-  file();
 }
+
+//Creating a spy profile through random JSONs informations
 function generateSpyProfile() {
   let instrument = random(instrumentData.instruments);
   spyProfile.alias = `The ${instrument}`;
@@ -124,23 +153,31 @@ function generateSpyProfile() {
   let card = random(tarotData.tarot_interpretations);
   spyProfile.password = random(card.keywords);
 
+  //Save th profile
   localStorage.setItem(KEY_PROFILE_DATA, JSON.stringify(spyProfile));
 }
-// draw()
-//
-// Description of draw() goes here.
+
+//Creating states
 function draw() {
   background(0);
+
+  //Calls file if state = file
   if (state === "file") {
     file();
 
     console.log(file);
-  } else if (state === "disappointment") {
+  }
+
+  //Calls disppointment if state = disappointment
+  else if (state === "disappointment") {
     disappointment();
     console.log(disappointment);
   }
 }
+
+//Creating file dispaly
 function file() {
+  //Display spy profile
   let profile = `**CONFIDENTIAL SPY PROFILE**;
   Name:${spyProfile.name}
   Alias:${spyProfile.alias}
@@ -148,6 +185,8 @@ function file() {
   Power:${spyProfile.power}
   Secret Weapon:${spyProfile.secretWeapon}
   Password:${spyProfile.password}`;
+
+  //Display text
   push();
   textFont(`Courier, monospace`);
   textSize(32);
@@ -156,9 +195,10 @@ function file() {
   text(profile, 50, 50);
   pop();
 }
-function disappointment() {
-  state = `disappointment`;
 
+//Created disappointment state
+function disappointment() {
+  //Added text
   push();
   textFont(`Courier, monospace`);
   textSize(32);
@@ -167,6 +207,8 @@ function disappointment() {
   text(disappointed, 0, 0);
   pop();
 }
+
+//Change the line in the dialog thorugh mousePressed
 function mousePressed() {
   currentLine = currentLine + 1;
 
