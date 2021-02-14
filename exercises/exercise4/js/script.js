50bd1b898b1bbb209f7f941df1409a506c20f75d
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-02-14 18:16:30 -0500

    E4:Changed bubble image; added ocean background image; added Gloria Hallelujah font;added localStorage for score; added states; added title,instructions and ending states;trying to solve popping bubble error; changes state based on a popped bubble; moved pin motion in bubbleGame function

--- exercises/exercise4/js/script.js
+++ exercises/exercise4/js/script.js
@@ -6,33 +6,60 @@ Using hand tracking we turn the userâ€™s index finger into a pin on our programâ
  A bubble floats upward repeatedly and the user can pop the bubble with the pointy end of their pin-finger.
 **************************************************/
 "use strict";
+let state = "title";
 let video = undefined;
 let modelName = `Handpose`;
 let handpose = undefined;
 let predictions = [];
+let bubbles = [];
+let intro = `Pop most bubbles as possible with your hand! Enjoy!`;
 let bubble;
-
+let pin;
+let hand;
+let index;
+let tip;
+let base;
+let tipX;
+let tipY;
+let baseX;
+let baseY;
+let bubbleImage;
+let ocean;
+let numBubbles = 10;
+let gameData = {
+  highScore: 0, // Start the high score at 0 by default
+};
 // setup()
 //
 // Description of setup() goes here.
+function preload() {
+  bubbleImage = loadImage(`assets/images/bubble.png`);
+  ocean = loadImage(`assets/images/ocean.jpg`);
+}
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(windowWidth, windowHeight);
   video = createCapture(VIDEO);
   video.hide();
-  handpose = ml5.handpose(video, { flipHorizontal: true }, function () {
-    console.log(`Model loaded.`);
-  });
+  handpose = ml5.handpose(video, { flipHorizontal: true }, function () {});
   handpose.on(`predict`, function (results) {
-    console.log(results);
     predictions = results;
   });
-
+  // Try to load the game data (remembering to parse it first)
+  let data = JSON.parse(localStorage.getItem(`game-data`));
+  // Check if there's anything there
+  if (data !== null) {
+    // There is data! So replace our default game data with the save data
+    gameData = data;
+  }
   bubble = {
     x: random(width),
     y: height,
-    size: 100,
+    w: 100,
+    h: 100,
+    image: bubbleImage,
     vx: 0,
-    vy: -2,
+    vy: -5,
+    active: true,
   };
 }
 
@@ -40,33 +67,112 @@ function setup() {
 //
 // Description of draw() goes here.
 function draw() {
-  background(0);
-  if (predictions.length > 0) {
-    let hand = predictions[0];
-    let index = hand.annotations.indexFinger;
-    let tip = index[3];
-    let base = index[0];
-    let tipX = tip[0];
-    let tipY = tip[1];
-    let baseX = base[0];
-    let baseY = base[1];
+  background(ocean);
+  textFont("Gloria Hallelujah");
+  if (state === "title") {
+    title();
+  } else if (state === "instructions") {
+    instructions();
+  } else if (state === "simulation") {
+    simulation();
+  } else if (state === "correct") {
+    rightAnswer();
+  } else if (state === "wrong") {
+    wrongAnswer();
+  }
+}
+function title() {
+  push();
 
-    displayPin(baseX, baseY, tipX, tipY);
+  //Adding title string
+  textSize(105);
+  fill(255);
+  textAlign(CENTER, CENTER);
+  text("Pop the Bubble!", width / 2, height / 2);
+
+  //adding how to change the state
+  push();
+  textSize(35);
+  text("Pop the bubble to start", width / 2, (2 * height) / 3);
+  pop();
+  bubbleGame();
+  if (!bubble.active) {
+    state = "instructions";
+  }
+  pop();
+}
+function instructions() {
+  push();
+
+  //Adding title string
+  textSize(55);
+  fill(255);
+  textAlign(LEFT, TOP);
+  text(intro, 10, 10, width / 2, height / 2);
+
+  //adding how to change the state
+  push();
+  textSize(35);
+  text("Pop the bubble to start", width / 2, (2 * height) / 3);
+  pop();
+  pop();
+  bubbleGame();
+  if (!bubble.active) {
+    state = "simulation";
+  }
+}
+function simulation() {
+  for (let i = 0; i < numBubbles; i++) {
+    // Create a fish
+    let fish = createFish(random(0, width), random(0, height));
+    // Add the fish to our array
+    school.push(fish);
+  }
+  bubbleGame();
+  if (!bubble.active > gameData.highScore) {
+    // Set the new high score
+    gameData.highScore = clicks;
+    // Save the game data to remember for next time, remembering to stringify the data first
+    localStorage.setItem(`game-data`, JSON.stringify(gameData));
+  }
+}
+function bubbleGame() {
+  if (predictions.length > 0) {
+    hand = predictions[0];
+    index = hand.annotations.indexFinger;
+    tip = index[3];
+    base = index[0];
+    tipX = tip[0];
+    tipY = tip[1];
+    baseX = base[0];
+    baseY = base[1];
 
     poppingBubble(tipX, tipY);
+    displayPin(baseX, baseY, tipX, tipY);
   }
 
   bubble.x += bubble.vx;
   bubble.y += bubble.vy;
-
-  displayBubble();
   resetBubble();
+  displayBubble();
+}
+function ending() {
+  // Display high score
+  push();
+  textSize(32);
+  textAlign(LEFT, TOP);
+  textStyle(BOLD);
+  fill(255, 255, 0);
+  text(`High score: ${gameData.highScore}`, 0, 0);
+  pop();
 }
 function poppingBubble(tipX, tipY) {
   let d = dist(tipX, tipY, bubble.x, bubble.y);
-  if (d < bubble.size / 2) {
-    bubble.x = random(0, width);
-    bubble.y = height;
+  if (d < bubble.w / 2 && d < bubble.h / 2) {
+    bubble.active = false;
+    console.log(d);
+    // bubble.x = random(0, width);
+    // bubble.y = height;
   }
 }
 function resetBubble() {
@@ -91,8 +197,9 @@ function displayPin(baseX, baseY, tipX, tipY) {
 }
 function displayBubble() {
   push();
-  fill(0, 100, 200);
+
   noStroke();
-  ellipse(bubble.x, bubble.y, bubble.size);
+  image(bubble.image, bubble.x, bubble.y, bubble.w, bubble.h);
+
   pop();
 }
