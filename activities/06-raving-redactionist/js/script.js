ea36f86b0518433c84b1da701746ada53401170d
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-03-08 12:25:56 -0500

    A5:Started Raving Redactionist activity

---
+++ activities/06-raving-redactionist/js/script.js
@@ -0,0 +1,18 @@
+/**************************************************
+Activity 05: Raving Redactionist 
+Cassandra Rousseau
+
+We will create a webpage of text with some passages “redacted” (covered in black bars).
+Over time the redactions will disappear, revealing the shocking secret text beneath them.
+The user is in charge of keeping the information secret, so they click the secret text to restore the redaction
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
