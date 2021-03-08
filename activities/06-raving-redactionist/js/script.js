c21fb4dd3fae2df84edb749dd91b257727b4f38f
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-03-08 13:29:56 -0500

    A6:Finished the activity

--- activities/06-raving-redactionist/js/script.js
+++ activities/06-raving-redactionist/js/script.js
@@ -1,18 +1,28 @@
 /**************************************************
-Activity 05: Raving Redactionist 
+Activity 05: Raving Redactionist
 Cassandra Rousseau
 
 We will create a webpage of text with some passages “redacted” (covered in black bars).
 Over time the redactions will disappear, revealing the shocking secret text beneath them.
 The user is in charge of keeping the information secret, so they click the secret text to restore the redaction
 **************************************************/
+"use strict";
 
-// setup()
-//
-// Description of setup() goes here.
-function setup() {}
+$(`.top-secret`).on(`click`, redact);
+setInterval(revelation, 500);
 
-// draw()
-//
-// Description of draw() goes here.
-function draw() {}
+function redact(event) {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+function attemptReveal() {
+  let r = Math.random();
+  if (r < 0.1) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
