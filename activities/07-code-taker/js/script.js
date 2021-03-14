aa04317e844cbc439c0547e4a7c79aa45c20ad3a
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-03-14 15:01:04 -0400

    A7:Finished the activity

--- activities/07-code-taker/js/script.js
+++ activities/07-code-taker/js/script.js
@@ -4,17 +4,27 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
-
-// setup()
-//
-// Description of setup() goes here.
-function setup() {
-
-}
-
-// draw()
-//
-// Description of draw() goes here.
-function draw() {
-
-}
+"use strict";
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function () {
+      $(this).dialog(`close`);
+    },
+  },
+});
+$(`.secret`).one(`mouseover`, function (event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({ helper: `clone` });
+});
+$(`#answer`).droppable({
+  drop: function (event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
