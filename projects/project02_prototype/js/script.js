5c2047c0bc277561a623522d34c6f921db362598
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-03-20 22:01:21 -0400

    P2 prototype: Started prototype

---
+++ projects/project02_prototype/js/script.js
@@ -0,0 +1,16 @@
+/**************************************************
+Project 02: Anything(prototype)
+Cassandra Rousseau
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {}
