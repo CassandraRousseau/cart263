284edc097b825ae2a0b2fd96bf636dd70e46bbc2
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-03-24 15:31:28 -0400

    E7:Edited css of words box, answers and found words; added comments;  added attributes to each answer; changed paragraphs of secret words to span; created a conditional checking if the right word is at its place; moved found class from secret.one to conditional where words are at their right place; edited conditional that checks if the whole poem is answered correctly

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -4,28 +4,43 @@ Cassandra Rousseau
 
 The user reads a poem and tries to fill the blanks with the words at the bottom of the poem.
 If they drag the words in the correct order into a special solution area,
-they complete the poem!
+they completed the poem!
 **************************************************/
 "use strict";
+
+// The dialog box opening once you complete the poem
 $(`#solved-dialog`).dialog({
   autoOpen: false,
+  // Response button
   buttons: {
     "I know.": function () {
       $(this).dialog(`close`);
     },
   },
 });
+
+// The draggable filling words
 $(`.secret`).one(`mouseover`, function (event) {
-  $(this).addClass(`found`, 500);
   $(this).draggable({ helper: `clone` });
 });
+
+// The areas that are need to be filled with words
 $(`.answer`).droppable({
   drop: function (event, ui) {
     let word = ui.draggable.text();
-    $(this).append(word);
-    ui.draggable.draggable(`disable`);
-    ui.draggable.removeClass(`found`);
-    if ($(this).text() === `Theremin`) {
+
+    // Put the word in the blank space if it's correct
+    if (word === $(this).attr("correct")) {
+      $(this).text(word);
+      $(this).removeClass("answer");
+      ui.draggable.draggable(`disable`);
+
+      // Colors the word in the words box area if correctly placed
+      ui.draggable.addClass(`found`, 500);
+    }
+
+    // Open dialog box if text filled completely and correctly
+    if ($(".answer").length === 0) {
       $(`#solved-dialog`).dialog(`open`);
     }
   },
