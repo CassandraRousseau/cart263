a8d1e14616f488319ecf7f3bce38d938a25bca1a
CassandraRousseau <70165552+CassandraRousseau@users.noreply.github.com>
2021-04-29 09:41:19 -0400

    P2: added capitalized "P" in "Part" in intro images titles; added gameMenu in Intro Scene

--- projects/project02/README.md
+++ projects/project02/README.md
@@ -1,3 +1,7 @@
-# Template p5 project
+# Project 2-Anything
 
-This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
+##Flufluf-Cassandra Rousseau
+
+_Flufluf_ is a platformer game about a cloud that needs to save the baby clouds from the evil clan of clouds. I wanted the players to
+
+Your final project folder should include a README.md document in the main folder (e.g. projects/project2/final/README.md) that contains a brief artist’s statement about your project. This statement should succinctly explain your artistic vision, the desired user experience, and how you believe you accomplished this both technically and artistically. This will help when evaluating the level of creativity you have brought to the project.
--- projects/project02/js/Intro.js
+++ projects/project02/js/Intro.js
@@ -7,32 +7,32 @@ class Intro extends Phaser.Scene {
   }
   create() {
     this.menu = this.add.image(400, 300, "gameMenu").setInteractive();
-    this.menu.on("pointerdown", this.createB, this);
+    this.menu.on("pointerdown", this.createA, this);
     this.musicIntro = this.sound.add("themeIntro");
     this.musicIntro.loop = true;
     this.musicIntro.play();
   }
-  createB() {
+  createA() {
     this.intro1 = this.add.image(400, 300, "introPart1").setInteractive();
-    this.intro1.on("pointerdown", this.createC, this);
+    this.intro1.on("pointerdown", this.createB, this);
+  }
+  createB() {
+    this.intro2 = this.add.image(400, 300, "introPart2").setInteractive();
+    this.intro2.on("pointerdown", this.createC, this);
   }
   createC() {
-    this.intro2 = this.add.image(400, 300, "introPart2").setInteractive();
-    this.intro2.on("pointerdown", this.createD, this);
+    this.intro3 = this.add.image(400, 300, "introPart3").setInteractive();
+    this.intro3.on("pointerdown", this.createD, this);
   }
   createD() {
-    this.intro3 = this.add.image(400, 300, "introPart3").setInteractive();
-    this.intro3.on("pointerdown", this.createE, this);
+    this.intro4 = this.add.image(400, 300, "introPart4").setInteractive();
+    this.intro4.on("pointerdown", this.createE, this);
   }
   createE() {
-    this.intro4 = this.add.image(400, 300, "introPart4").setInteractive();
-    this.intro4.on("pointerdown", this.createF, this);
+    this.intro5 = this.add.image(400, 300, "introPart5").setInteractive();
+    this.intro5.on("pointerdown", this.createF, this);
   }
   createF() {
-    this.intro5 = this.add.image(400, 300, "introPart5").setInteractive();
-    this.intro5.on("pointerdown", this.createG, this);
-  }
-  createG() {
     this.musicIntro.stop();
     this.scene.start("level2");
   }
